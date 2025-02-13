6b006d8d50ce8e93e52ff1c298dbe091f342bc54
Tsykunov Oleksandr <50213824+AlexThukynov1@users.noreply.github.com>
2025-02-13 16:57:04 +0200

    Add files via upload

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,28 @@
+<template>
+  <main>
+    <form action="" @submit.prevent="addRestaurant">
+      <label for="restaurent-name">Restaurant name</label>
+      <input type="text" name="" id="restaurent-name" v-model="newRestaurantName" />
+      <button type="submit">Add Restaurant</button>
+    </form>
+    <ul>
+      <li v-for="restaurant of restaurantList" :key="restaurant">
+        {{ restaurant }}
+      </li>
+    </ul>
+  </main>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const newRestaurantName = ref<string>('')
+const restaurantList = ref<string[]>([])
+
+function addRestaurant() {
+  restaurantList.value.push(newRestaurantName.value)
+  newRestaurantName.value = ''
+}
+</script>
+
+<style scoped></style>
